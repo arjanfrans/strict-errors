e40b1106ce0387727958bf4bee8323d3c37938e2
Arjan Frans <arjanfrans.com@gmail.com>
2016-06-29 00:03:10 +0200

    Remove dist from src

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var createErrorDefinitions = require('./error-definitions');
-var createError = require('./create-error');
-
-module.exports = {
-    createErrorDefinitions: createErrorDefinitions,
-    createError: createError
-};
