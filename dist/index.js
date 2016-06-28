cd1253d599746cca3cab346731756e9da72f5a80
Arjan Frans <arjanfrans.com@gmail.com>
2016-06-29 00:01:33 +0200

    Use babel

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var createErrorDefinitions = require('./error-definitions');
+var createError = require('./create-error');
+
+module.exports = {
+    createErrorDefinitions: createErrorDefinitions,
+    createError: createError
+};
